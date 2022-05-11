6bd3611c90bc9189eb5d4da0e5e6f84f1af78af0
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-05-11 11:59:01 +0530

    initial commit

---
+++ MyTemplate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<cmath>
+#include<map>
+#include<set>
+#define fio ios::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    string s;
+#endif
+
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Competitive Programming / Data Structures and Algorithm
+This repo contains my practice codes of dsa/cp on various platforms.# cp-dsa
