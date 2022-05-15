8d4c98d54fd50ee2eca69f1e6d8a51492a592de0
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-05-15 19:01:44 +0530

    implementation 800

--- Codeforces/263A_Beautiful Matrix.cpp
+++ Codeforces/263A_Beautiful Matrix.cpp
@@ -18,9 +18,19 @@ int main() {
 #endif
 
 	fio;
-	int t;
-	cin >> t;
-	while (t--) {
+	vector<vector<int>> v(5, vector<int> (5));
+	int x = 0, y = 0;
 
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> v[i][j];
+			if (v[i][j] == 1) {
+				x = i + 1;
+				y = j + 1;
+			}
+		}
 	}
+
+	int ans = abs(3 - x) + abs(3 - y);
+	cout << ans << endl;
 }
