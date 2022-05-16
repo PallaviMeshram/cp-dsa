9705e59a2143bb53d87b7a4d4e9da3f568e9d33b
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-05-16 09:41:50 +0530

    array easy

---
+++ Leetcode/1920. Build Array from Permutation.cpp
@@ -0,0 +1,16 @@
+// Without Extra Space
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] + ((nums[nums[i]]) % n) * n;
+        } for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] / n;
+        }
+
+        return nums;
+    }
+};
