5143f860381ffd6283ba659d5157a8f12529f42c
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-05-20 22:27:55 +0530

    easy array

---
+++ Leetcode/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int cnt = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "X++" or operations[i] == "++X") cnt++;
+            else cnt--;
+        }
+        return cnt;
+    }
+};
