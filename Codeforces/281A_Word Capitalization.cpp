86281939b09c14a836c497adb14c125ab4930ced
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-06-03 10:01:21 +0530

    string 800

---
+++ Codeforces/281A_Word Capitalization.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<cmath>
+#include<map>
+#include<set>
+#define fio ios::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fio;
+	string s;
+	cin >> s;
+	s[0] = toupper(s[0]);
+	cout << s << endl;
+}
