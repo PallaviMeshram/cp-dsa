a5a9113bc63c6fbc285e5f38e5374cab56c80679
Pallavi Meshram <pallaviameshram2022@gmail.com>
2022-06-06 11:48:12 +0530

    implement 800

---
+++ Codeforces/492A_Vanya and Cubes.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<vector>
+#include<stack>
+#include<cmath>
+#include<map>
+#include<set>
+#define fio ios::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fio;
+	int n;
+	cin >> n;
+	int sum = 0;
+	int cnt = 0;
+	while (n >= sum) {
+		cnt++;
+		sum += ((cnt + 1) * cnt) / 2;
+	}
+	cout << cnt - 1 << endl;
+}
