816d55114d21619d8c724d7b16dae507256da60b
PallaviMeshram <pauvpam@gmail.com>
2022-10-12 15:18:09 +0530

    template or base snippet edited

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -12,10 +12,10 @@ using namespace std;
 
 int main() {
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
 
     fio;
     int t;
